617878d48b2dd79080c401fea85a4f5ba930b172
Tyler Johnson <tylerjohnson139@gmail.com>
2019-09-22 22:56:51 -0700

    add carInsurance project

---
+++ carInsurance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (Enter true or false)");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets have you had?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            Console.WriteLine((age > 15 && dui == false && tickets < 3));
+            Console.ReadLine();
+        }
+    }
+}
