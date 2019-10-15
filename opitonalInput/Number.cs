643df4adc6989837bf5c96396c8ed6fcd0f7bbbb
Tyler Johnson <tylerjohnson139@gmail.com>
2019-10-15 00:52:20 -0700

    add optionalInput

---
+++ opitonalInput/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opitonalInput
+{
+    class Number
+    {
+        public static int Nums(int userValue, int userValue2 = 32)
+        {
+            int newValues = userValue + userValue2;
+            return userValue + userValue2;
+        }
+    }
+}
---
+++ opitonalInput/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace opitonalInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number nums = new Number();
+            Console.WriteLine("Pick a number:");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick another number--optional");
+            var userValue2IsValid = int.TryParse(Console.ReadLine(), out int userValue2);
+
+            int result;
+            if (userValue2IsValid)
+            {
+                result = Number.Nums(userValue, userValue2);
+            }
+            else
+            {
+                result = Number.Nums(userValue);
+            }
+
+            Console.WriteLine("Your total is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
