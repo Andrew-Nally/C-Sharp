bd0727970bf6503bff4c19926ce68c88232a13b9
Tyler Johnson <tylerjohnson139@gmail.com>
2019-10-14 22:46:24 -0700

    add numProject

---
+++ numProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace numProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            userNumbers number = new userNumbers();
+            userNumbers.InputNumbers(12);
+            userNumbers.DecimalNum(35.6);
+            userNumbers.StringNum("4");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ numProject/userNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace numProject
+{
+    class userNumbers
+    {
+        public static void InputNumbers(int i)
+        {
+            int MultiplyI = i * i;
+            Console.WriteLine(i + " multiplied by itself is: " + MultiplyI);
+        }
+
+        public static void DecimalNum(double i)
+        {
+            double addI = i + 34.75;
+            int decNum = Convert.ToInt32(addI);
+            Console.WriteLine(i + " added to 34.75, converted to an integer, is: " + decNum);
+        }
+
+        public static void StringNum(string i)
+        {
+            int stringConvert = Convert.ToInt32(i);
+            int numString = stringConvert / 2;
+            Console.WriteLine(i + " divided by 2 is: " + numString);
+        }
+    }
+}
