2bb4d031345712b4df329027b8261566a7bad28f
Tyler Johnson <tylerjohnson139@gmail.com>
2019-10-14 21:45:57 -0700

    add twoNums

---
+++ twoNums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twoNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            numberInput number = new numberInput();
+            Console.WriteLine("Please enter a whole number:");
+            string numA = Console.ReadLine();
+            int numAC = Convert.ToInt32(numA);
+            Console.WriteLine("Please enter another whole number:");
+            string numB = Console.ReadLine();
+            int numBC = Convert.ToInt32(numB);
+            numberInput.userNumbers(numAC, numBC);
+            numberInput.userNumbers(30, 24);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ twoNums/numberInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twoNums
+{
+    class numberInput
+    {
+        public static void userNumbers(int num1, int num2)
+        {
+            int num1Add = num1 * 10;
+            Console.WriteLine(num1 + " multiplied by 10 is: " + num1Add + ". The second number you entered is: " + num2);
+        }
+    }
+}
