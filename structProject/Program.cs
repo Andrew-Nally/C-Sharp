bb388e3207185670530fc6971ff21ff85ad580d8
Tyler Johnson <tylerjohnson139@gmail.com>
2019-10-27 01:49:35 -0700

    add structProject

---
+++ structProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structProject
+{
+    class Program
+    {
+        public struct Number
+        {
+            public double Amount { get; set; }
+
+        }
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 34.5;
+
+            Console.WriteLine("Amount = " + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
