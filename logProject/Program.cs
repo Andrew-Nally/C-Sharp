5895a1fb48a6e185e351d3264f7a37f2ac267d0b
Tyler Johnson <tylerjohnson139@gmail.com>
2019-11-13 12:42:07 -0800

    add logProject

---
+++ logProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace logProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number. Any number");
+            string numChoice = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\Tyler\Desktop\Test\test.txt", numChoice);
+            string text = File.ReadAllText(@"C:\Users\Tyler\Desktop\Test\test.txt");
+            Console.WriteLine("Contents of test.txt = {0}", text);
+            Console.ReadLine();
+        }
+    }
+}
