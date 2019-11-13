f567c48b70dd46288f56f3ee7cd98ffbdac833a5
Tyler Johnson <tylerjohnson139@gmail.com>
2019-11-13 13:10:14 -0800

    add dateTimeProject

--- TwentyOne/TwentyOne/Dealer.cs
+++ TwentyOne/TwentyOne/Dealer.cs
@@ -20,6 +20,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\Tyler\Log\Log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
---
+++ dateTimeProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTimeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currTime = DateTime.Now;
+            Console.WriteLine("It is currently: " + currTime);
+
+            Console.WriteLine("\nEnter a whole number:");
+            var userNum = Convert.ToInt32(Console.ReadLine());
+
+            currTime = currTime.AddHours(userNum);
+            Console.WriteLine("\nIn {0} hours will be: {1}.", userNum, currTime);
+            Console.ReadLine();
+        }
+    }
+}
